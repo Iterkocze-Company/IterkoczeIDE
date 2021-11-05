7920bd7318a2597202a803230622a92ac506ed37
Michał <m12@onet.com.pl>
2021-11-05 20:51:17 +0100

    Initial commit

---
+++ IterkoczeIDE/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace IterkoczeIDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Code = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompilatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompilujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompilujIUruchomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Code)).BeginInit();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Code
+            // 
+            this.Code.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.Code.AutoIndent = false;
+            this.Code.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>.+)\r\n";
+            this.Code.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.Code.BackBrush = null;
+            this.Code.BackColor = System.Drawing.Color.Silver;
+            this.Code.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.Code.CharHeight = 14;
+            this.Code.CharWidth = 8;
+            this.Code.CommentPrefix = "--";
+            this.Code.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Code.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.Code.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Code.Hotkeys = resources.GetString("Code.Hotkeys");
+            this.Code.IsReplaceMode = false;
+            this.Code.Language = FastColoredTextBoxNS.Language.Lua;
+            this.Code.LeftBracket = '(';
+            this.Code.LeftBracket2 = '{';
+            this.Code.Location = new System.Drawing.Point(0, 24);
+            this.Code.Name = "Code";
+            this.Code.Paddings = new System.Windows.Forms.Padding(0);
+            this.Code.RightBracket = ')';
+            this.Code.RightBracket2 = '}';
+            this.Code.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.Code.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("Code.ServiceColors")));
+            this.Code.Size = new System.Drawing.Size(800, 426);
+            this.Code.TabIndex = 0;
+            this.Code.Zoom = 100;
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.kompilatorToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 24);
+            this.menu.TabIndex = 1;
+            this.menu.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileToolStripMenuItem,
+            this.openFileToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.plikToolStripMenuItem.Text = "&File";
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.newFileToolStripMenuItem.Text = "&New file";
+            this.newFileToolStripMenuItem.Click += new System.EventHandler(this.newFileToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.openFileToolStripMenuItem.Text = "&Open file";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // kompilatorToolStripMenuItem
+            // 
+            this.kompilatorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kompilujToolStripMenuItem,
+            this.kompilujIUruchomToolStripMenuItem});
+            this.kompilatorToolStripMenuItem.Name = "kompilatorToolStripMenuItem";
+            this.kompilatorToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.kompilatorToolStripMenuItem.Text = "&Compiler";
+            // 
+            // kompilujToolStripMenuItem
+            // 
+            this.kompilujToolStripMenuItem.Name = "kompilujToolStripMenuItem";
+            this.kompilujToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kompilujToolStripMenuItem.Text = "C&ompile";
+            this.kompilujToolStripMenuItem.Click += new System.EventHandler(this.kompilujToolStripMenuItem_Click);
+            // 
+            // kompilujIUruchomToolStripMenuItem
+            // 
+            this.kompilujIUruchomToolStripMenuItem.Name = "kompilujIUruchomToolStripMenuItem";
+            this.kompilujIUruchomToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kompilujIUruchomToolStripMenuItem.Text = "Compile and &run";
+            this.kompilujIUruchomToolStripMenuItem.Click += new System.EventHandler(this.kompilujIUruchomToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Code);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "Iterkocze IDE";
+            ((System.ComponentModel.ISupportInitialize)(this.Code)).EndInit();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox Code;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kompilatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kompilujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kompilujIUruchomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+    }
+}
+
---
+++ IterkoczeIDE/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastColoredTextBoxNS;
+using System.Windows.Forms;
+
+namespace IterkoczeIDE
+{
+    public partial class Form1 : Form
+    {
+        private static bool isSaved;
+        public static string fileName;
+        public static string filePath;
+        public static bool isOpened = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Style s = new TextStyle(Brushes.Green, Brushes.Silver, FontStyle.Bold);
+            Code.AddStyle(s);
+        }
+
+        private void newFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fd = new();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = fd.FileName;
+                filePath = Path.GetDirectoryName(fileName);
+                isSaved = true;
+                isOpened = true;
+                
+                string buffer = "";
+                foreach (string line in Code.Lines)
+                {
+                    if (line.StartsWith(" "))
+                    {
+                        buffer += line.TrimStart().Insert(0, "\t") + "\n";
+                    }
+                    else
+                        buffer += line + "\n";
+                }
+                File.WriteAllText(filePath, buffer);
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = fd.FileName;
+                filePath = Path.GetDirectoryName(fileName);
+                isSaved = false;
+                isOpened = true;
+                Code.Text = File.ReadAllText(fd.FileName);
+            }
+        }
+
+        private void kompilujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new();
+            info.WorkingDirectory = filePath;
+            info.FileName = "ippCompiler.exe";
+            info.Arguments = fileName;
+
+            Process.Start(info);
+        }
+
+        private void kompilujIUruchomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new();
+            info.WorkingDirectory = filePath;
+            info.FileName = "ippCompiler.exe";
+            info.Arguments = fileName + " -run";
+
+            Process.Start(info);
+        }
+    }
+}
